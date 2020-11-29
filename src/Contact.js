9bab1b5eada6426fcffe987b58c1f573b428ab5f
Ana Caballero <ana.caballero@gmx.de>
2020-11-29 11:51:40 +0100

    change opacity format, adjust form settings

--- src/Contact.js
+++ src/Contact.js
@@ -12,11 +12,11 @@ export default function Contact () {
             </div>
             <div className="d-flex justify-content-center"><ContactButtons /></div>
             <div className="mt-4 mb-5 d-flex justify-content-center form-container">
-                <form name="contact" method="POST" data-netlify="true" className="contact-form">
+                <form name="contact" method="POST" data-netlify="true" content-type="application/x-www-form-urlencoded" className="contact-form">
                     <Row className="d-flex justify-content-center"><input type="text" name="name" placeholder="Name*"></input></Row>
                     <Row className="d-flex justify-content-center"><input type="email" name="email" placeholder="Email*"></input></Row>
                     <Row className="d-flex justify-content-center"><textarea name="message" placeholder="Message*" rows="10"></textarea></Row>
-                    <Row className="d-flex justify-content-center"><input type="submit" value="Submit" className="submit-button"></input></Row>
+                    <Row className="d-flex justify-content-center"><button type="submit" className="submit-button">Submit</button></Row>
                 </form>
             </div>
         </section>
